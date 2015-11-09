93bbb45387050668d646fdad66c5d694a2d33a06
Klinton Bicknell <kbicknell@northwestern.edu>
2015-11-09 11:32:03 -0600

    add bin_int interval

--- R/ggintervals.R
+++ R/ggintervals.R
@@ -14,3 +14,19 @@ mean_cl_boot_bca <- function(x, conf.int = 0.95, replicates = 1000) {
   result <- boot::boot.ci(boot_out, type = "bca")
   dat <- data.frame(y = mean(x), ymin = result$bca[4], ymax = result$bca[5])
 }
+
+#' creates a confidence level for the mean of independent binary data, using the
+#' exact test (which yields intervals that are a little conservative for small
+#' sample sizes, but guaranteed to capture >= 0.95 probability)
+#' 
+#' @param x a vector of (numeric) binary 0s and 1s
+#' @param conf.int the size of the confidence interval
+#'   
+#' @export
+#' 
+bin_int <- function(x, conf.int = 0.95) {
+  x <- x[!is.na(x)]
+  stopifnot(is.numeric(x))
+  interval <- binom.test(sum(x), length(x), conf.level = conf.int)$conf.int
+  dat <- data.frame(y = mean(x), ymin = interval[1], ymax = interval[2])
+}
